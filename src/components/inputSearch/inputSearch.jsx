f0141fc0c9de5c87bf6d6f596ecddee3b051a8ed
TRAHIM86 <trahim1986@gmail.com>
2025-12-21 20:57:04 +0300

    feat: added button cleanStr (appears with text)

--- src/components/inputSearch/inputSearch.jsx
+++ src/components/inputSearch/inputSearch.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
+import styles from "./inputSearch.module.css";
 
 export const InputSearch = ({ searchStr, setSearchStr, setCurrentPage }) => {
   return (
-    <input
-      type="text"
-      value={searchStr}
-      placeholder="Search..."
-      onChange={(e) => {
-        setSearchStr(e.target.value);
-        setCurrentPage(1);
-      }}
-    />
+    <div className={styles.inputBlock}>
+      <input
+        className={styles.searchField}
+        type="text"
+        value={searchStr}
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearchStr(e.target.value);
+          setCurrentPage(1);
+        }}
+      />
+      <div className={styles.divClear} onClick={() => setSearchStr("")}>
+        {searchStr && "X"}
+      </div>
+    </div>
   );
 };
